e32b3012d4bc91de2e0971ba14fb85d91a7966f9
Richard Allen <Richard.Allen@ubisoft.com>
2024-10-21 10:32:02 +0100

    blank project setup

---
+++ quadtreeexamples.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//#include <catch2/catch_test_macros.hpp>
+//#include <catch2/benchmark/catch_benchmark.hpp>
+//
+//TEST_CASE("Quadtree - Unit Tests")
+//{
+//}
+//
+//TEST_CASE("Quadtree - Benchmarks")
+//{
+//    BENCHMARK("Benchmark")
+//    {
+//    };
+//}
